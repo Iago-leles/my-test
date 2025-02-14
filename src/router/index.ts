283db07e8f6c23d660fb37f39fb03e83eefb2cfc
iago.leles <iago.leles@gmail.com>
2025-02-14 19:09:34 -0300

    fix routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,5 @@
-import { defineRouter } from '#q-app/wrappers'
-import {
-  createMemoryHistory,
-  createRouter,
-  createWebHashHistory,
-  createWebHistory,
-} from 'vue-router'
+import { route } from 'quasar/wrappers'
+import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
 /*
@@ -16,21 +11,11 @@ import routes from './routes'
  * with the Router instance.
  */
 
-export default defineRouter(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : process.env.VUE_ROUTER_MODE === 'history'
-      ? createWebHistory
-      : createWebHashHistory
-
+export default route(function (/* { store, ssrContext } */) {
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
-
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.VUE_ROUTER_BASE),
+    history: createWebHistory(),
   })
 
   return Router
